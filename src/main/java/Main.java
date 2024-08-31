08f94c4dc618e8ecdd208df3e1fd4fba8ef616fd
Gabrielly Cristina <167761882+GabT1na@users.noreply.github.com>
2024-08-31 22:44:26 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+  public static void main(String[] args) {
+    String nome = "Gabrielly";
+    int dia = 27;
+    int mes = 10;
+    int ano = 2009;
+
+    double pi = 3.141592;
+    
+    //System.out.printf("nasceu dia %d do mês %d do ano de %d", dia, mes, ano);
+    System.out.printf("O valor de Pi é: %.2f", pi);
+  }
+}
